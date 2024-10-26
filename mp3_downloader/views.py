9f75972e657ceb1cf1674cb78208f021049ec860
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2024-10-26 15:52:08 +0530

    Update views.py

--- mp3_downloader/views.py
+++ mp3_downloader/views.py
@@ -23,7 +23,11 @@ def download_audio(request):
                         'preferredcodec': 'mp3',
                         'preferredquality': '192',
                     }],
-                    'quiet': False,  # To show download progress
+                    'nocheckcertificate': True,
+                    'quiet': False,  
+                    'headers': {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                    },
                 }
 
                 # Use a context manager to handle yt-dlp
