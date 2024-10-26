dd465288ae15750285373ac0ccf1e5978af08a2a
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2024-10-26 14:55:21 +0530

    Delete mp3_downloader directory

--- mp3_downloader/mp3_downloader/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from mp3_downloader import views
-
-urlpatterns = [
-    path('', views.index),
-    path('download_audio/', views.download_audio, name='download_audio'),
-]
